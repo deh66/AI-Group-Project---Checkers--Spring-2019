b251327ab76b11ab7df01a553754ebae09f0c4b9
Daniel Hollo <deh66@zips.uakron.edu>
2019-04-01 18:02:08 -0400

    Modifies .gitignore, adds Source directory, which is where future source code will be placed.

---
+++ Source/Source.cpp
@@ -0,0 +1,24 @@
+/******************************************************
+* Artificial Intelligence and Heuristic Programming
+* 3460:460-001 Spring 2019
+* Group Project: Checkers AI
+*
+* Daniel Hollo
+* deh66@zips.uakron.edu
+* 2980428
+*
+* Ben Hawk
+* bih9@zips.uakron.edu
+* [#######]
+******************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World";
+
+	return 0;
+}
