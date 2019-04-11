732b2ebad0e730cd5f110d1c2926d64fe82e94ad
Daniel Hollo <deh66@zips.uakron.edu>
2019-04-11 19:13:25 -0400

    Adds our working files for our source code.

---
+++ checkers-master/src/checkers/D_Tree.java
@@ -0,0 +1,12 @@
+/**
+ * @file D_Tree.java
+ * 
+ * @author Daniel Hollo
+ * @author Benjamin Hawk
+ * @date 4/11/2019
+ * 
+ * @breif This file contains the implementation of a Decision Tree based AI.
+ * 
+ * 	Contains a custom built Decision Tree algorithm using board state 
+ * 		spaces to determine AI moves.
+*/
---
+++ checkers-master/src/checkers/Minimax.java
@@ -0,0 +1,12 @@
+/**
+ * @file Minimax.java
+ * 
+ * @author Daniel Hollo
+ * @author Benjamin Hawk
+ * @date 4/11/2019
+ * 
+ * @breif This file contains the implementation of a Minimax based AI.
+ * 
+ * 	Contains a custom built Minimax algorithm using board state 
+ * 		spaces to determine AI moves.
+*/
